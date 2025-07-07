a0ba62bd16682ffac086c78af59a229697e805c8
Lance <lance.barreto05@gmail.com>
2025-07-07 14:10:08 +0530

    CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,18 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://strita.netlify.app'], // replace with your actual frontend URLs
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: '*',
+      exposeHeaders: ['Content-Disposition', 'Content-Type'],
+      credentials: true,
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
